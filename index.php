f679074f600d9094f720a9719aa8146bfca1054b
sandalhathat <sandalhathat@gmail.com>
2022-06-17 13:27:03 -0500

    adding variables

--- index.php
+++ index.php
@@ -5,8 +5,12 @@
   </head>
   <body>
     <?php
+    $name = "PHP Store";
+    $credit = 1000;
     // this is how to comment in php
-    echo "<h1>Welcome!</h1>";
+    // echo "<h1>Welcome!</h1>";
+    echo "<h1>Welcome to ".$name."!</h1>";
+    echo "<h2>You have $".$credit." in your wallet.</h2>";
     ?>
   </body>
 </html>
