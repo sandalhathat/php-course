f6f29f44cfb1e4ebc4e9f90edcdd122bce411955
sandalhathat <sandalhathat@gmail.com>
2022-06-17 13:47:11 -0500

    added tax calc function

--- index.php
+++ index.php
@@ -31,6 +31,18 @@
             }
           }
 
+          $amount=800;
+          $taxRate=0.0825;
+          $addedTax= $amount*$taxRate;
+          echo $addedTax;
+
+          function tax_calc($amount,$tax) {
+            $calculate_tax = $amount*$tax;
+            $amount = round($amount+$calculate_tax,2);
+            return $amount;
+          }
+
+          echo tax_calc(750,0.223);
     ?>
   </body>
 </html>
