ae3900917eeccf13ff9f018df72a256c7df93dc7
sandalhathat <sandalhathat@gmail.com>
2022-06-17 12:34:43 -0500

    initial php setup

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP Store</title>
+  </head>
+  <body>
+    <?php
+    // this is how to comment in php
+    echo "<h1>Welcome!</h1>";
+    ?>
+  </body>
+</html>
